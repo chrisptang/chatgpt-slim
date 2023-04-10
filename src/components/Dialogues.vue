b601b99081ecf47487ba83c180eb67f3e087206c
tang peng <tangpeng@tangs-MacBook-Pro.local>
2023-04-10 16:17:03 +0800

    dialogue now works pretty well

--- src/components/Dialogues.vue
+++ src/components/Dialogues.vue
@@ -253,7 +253,7 @@ p.chat-propmt {
     }
 
     .dialogue-list {
-        width: 400px;
+        /* width: 400px; */
     }
 
     .dialogue-detail {
@@ -264,7 +264,7 @@ p.chat-propmt {
 
     .dialogue-container {
         width: 100vw;
-        max-width: 1440px;
+        max-width: 1280px;
     }
 }
 
