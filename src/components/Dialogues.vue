f8464b1acf339c061809c6b4e7b813d075cf8dab
tang peng <tangpeng@tangs-MacBook-Pro.local>
2023-04-24 10:00:59 +0800

    adding copy code button

--- src/components/Dialogues.vue
+++ src/components/Dialogues.vue
@@ -183,7 +183,7 @@ export default {
                     <li v-for="(message, index) in working_dialogue.messages" :key="message.content + index"
                         class="single-chat">
                         <p :class="message.role == 'user' ? 'chat-propmt' : 'chat-response'"
-                            v-html="message.role == 'user' ? message.content : window._renderMD(message.content)">
+                            v-html="message.role == 'user' ? message.content : window._renderMD(message.content, true)">
                         </p>
                     </li>
                 </ul>
